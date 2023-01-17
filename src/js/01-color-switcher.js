b89d7eb8a7dca99d9e09a5df43d985c03cbb340f
Konstantin2023 <pscosx@gmail.com>
2023-01-17 13:36:07 +0200

    9 дз, 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
-const gii = 100;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+let idInterval = null;
+
+refs.btnStart.addEventListener('click', onColorChangeStart);
+refs.btnStop.addEventListener('click', onColorChangeStop);
+
+function onColorChangeStart(evt) {
+  onBtnDisabled();
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onColorChangeStop(evt) {
+  onBtnDisabled();
+  clearInterval(idInterval);
+}
+
+function onBtnDisabled() {
+  if (!refs.btnStart.disabled) {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+  } else {
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+  }
+}
